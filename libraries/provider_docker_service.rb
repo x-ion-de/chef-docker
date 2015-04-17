9907cabf76e5f260a08279cbc261b3e1ad82bb76
Sean OMeara <sean@chef.io>
2015-04-17 00:07:32 -0400

    adding resource interface and provider action stubs

--- libraries/provider_docker_service.rb
+++ libraries/provider_docker_service.rb
@@ -1,6 +1,25 @@
 class Chef
   class Provider
     class DockerService < Chef::Provider::LWRPBase
+
+      # actions :create, :delete, :start, :stop, :restart, :enable
+      action :create do
+      end
+
+      action :delete do
+      end
+
+      action :start do
+      end
+
+      action :stop do
+      end
+
+      action :restart do
+      end
+
+      action :enable do
+      end
     end
   end
 end
--- libraries/resource_docker_service.rb
+++ libraries/resource_docker_service.rb
@@ -1,6 +1,17 @@
 class Chef
   class Resource
     class DockerService < Chef::Resource::LWRPBase
+      # Manually set the resource name because we're creating the classes
+      # manually instead of letting the resource/ and providers/
+      # directories auto-name things.
+      self.resource_name = :docker_service
+
+      # resource actions
+      actions :create, :delete, :start, :stop, :restart, :enable
+      default_action :create
+
+      # register with the resource resolution system
+      provides :docker_service
     end
   end
 end
