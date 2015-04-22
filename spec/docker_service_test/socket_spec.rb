f8026094b9799495ec545a5aea29e033828b9d96
Sean OMeara <sean@chef.io>
2015-04-22 19:39:31 -0400

    savegame: fixing spec to to point to socket. again.

--- spec/docker_service_test/socket_spec.rb
+++ spec/docker_service_test/socket_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'docker_service_testsocket on centos-7.0' do
+describe 'docker_service_test::socket on centos-7.0' do
   cached(:default) do
     ChefSpec::SoloRunner.new(
       platform: 'centos',
@@ -8,10 +8,10 @@ describe 'docker_service_testsocket on centos-7.0' do
     # step_into: 'docker_service'
     ) do |node|
       node.set['docker']['version'] = nil
-    end.converge('docker_service_testsocket')
+    end.converge('docker_service_test::socket')
   end
 
-  # Resource in docker_service_testsocket
+  # Resource in docker_service_test::socket
   context 'compiling the test recipe' do
     it 'creates docker_service[default]' do
       expect(default).to create_docker_service('default')
